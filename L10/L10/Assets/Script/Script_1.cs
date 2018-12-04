23e486ba6a56c8d7ec908462160deb90a72ecac4
Ligiart <OlegGutsev@users.noreply.github.com>
2018-12-04 20:18:26 +0300

    Gutsev Lab10
    Gutsev Lab10

---
+++ L10/L10/Assets/Script/Script_1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Floor")
+        {
+            Debug.Log("Hit the floor");
+        }
+        if (collision.gameObject.name == "Wall")
+        {
+            Debug.Log("Hit the wall");
+        }
+    }
+}
---
+++ L10/L10/Assets/Script/Script_5.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_5 : MonoBehaviour {
+    public float speed = 5;
+    private Rigidbody rb;
+    
+    private Color basicColor = Color.green;
+    private Color hoverColor = Color.red;
+    private Color hoverColor2 = Color.blue;
+    private Renderer renderer;
+
+    public Transform gameobject;
+   
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        renderer = GetComponent<Renderer>();
+        //renderer.material.color = basicColor;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            renderer.material.color = hoverColor;
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            renderer.material.color = hoverColor2;
+        }
+       
+      
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+
+ }
+
+     void OnCollisionEnter(Collision collision)
+    {
+       /* */
+
+       /* if (collision.gameObject.name == "Blue_Cube")
+        {
+            rb.AddForce(0, 1000, 0);
+        }
+         */
+        if (collision.gameObject.name == "Grenn_Cube")
+        {
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.name == "Red_Cube")
+        {
+            Destroy(this.gameObject);
+        }
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(0, 1000, 0);
+    }
+}
